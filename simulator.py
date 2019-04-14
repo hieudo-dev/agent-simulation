95589b1765300caa6c4db6ab16998406b5611902
Hieu Do Ngoc <hieudn.uh@gmail.com>
2019-04-14 19:18:47 -0400

    Add Simulator skeleton code and gitignore

--- simulator.py
+++ simulator.py
@@ -1,1 +1,17 @@
-import pyswip
+from pyswip import Prolog
+
+class Simulator:
+	env = [[]]
+
+	def __init__(self, N, M, dirtPercent, childCount, t):
+		self.board = [["" * M] for _ in range(N)]
+		# TODO: generar ambiente incial
+
+	def changeEnviroment(self):
+		# TODO: generar nuevo ambiente
+		pass
+		
+	def simulate(self, n):
+		for _ in range(n):
+			# TODO: realizar simulacion
+			pass
