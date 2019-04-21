a90a413ce6fa522e8f01ae633373ef417b4d09c4
Hieu Do Ngoc <hieudn.uh@gmail.com>
2019-04-21 18:10:05 -0400

    Add BFS algorithm to Robot Agent

--- robot_agent.pl
+++ robot_agent.pl
@@ -8,3 +8,29 @@ is_dirt(X, Y, Dirtlist):- member((X, Y), Dirtlist).
 inside_enviroment(X, Y, N, M):-
 	X>=0,X<N, 
 	Y>=0,Y<M.
+
+visited(Node, [LastPath|[]]):-member(Node, LastPath), !.
+visited(Node, [Path|Paths]):-
+	member(Node, Path);
+	visited(Node, Paths).
+
+expand([[Node|Path]|Paths], N, M, Obstacles, Extended):-
+	Node = (X, Y),
+	findall([(U,V), Node|Path], 
+				(adjacent(X, Y, U, V),
+				 inside_enviroment(U, V, N, M),
+				 not(member((U, V), Obstacles)),
+				 not(visited((U,V), [[Node|Path]|Paths])))
+				, Extended).
+
+% bfs(PathsQueue, Rows, Cols, Obstacles, Goals, Solution)
+% returns the first shortest path to a goal from the lis of goals
+bfs([[Node|Path]|_], _, _, _, GoalsList, Solution):-
+	member(Node, GoalsList),
+	reverse([Node|Path], Solution),
+	!.
+bfs(PathsQueue, N, M, Obstacles, GoalsList, Solution):-
+	PathsQueue = [Head|Paths],
+	expand(PathsQueue, N, M, Obstacles, Extended),
+	append(Paths, Extended, Path1),
+	bfs(Path1, N, M, Obstacles, GoalsList, Solution).
