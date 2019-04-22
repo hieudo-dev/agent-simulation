d9dd774467f7b219989bb07b986fa3cdc7c647f0
Hieu Do Ngoc <hieudn.uh@gmail.com>
2019-04-22 11:47:14 -0400

    Implement more predicates for the robot agent

--- robot_agent.pl
+++ robot_agent.pl
@@ -3,6 +3,12 @@ adjacent(X, Y, X1, Y):- X1 is X+1.
 adjacent(X, Y, X1, Y):- X1 is X-1.
 adjacent(X, Y, X, Y1):- Y1 is Y+1.
 adjacent(X, Y, X, Y1):- Y1 is Y-1.
+
+get_direction([(X, Y), (X+1, Y) |_], down).
+get_direction([(X, Y), (X-1, Y) |_], up).
+get_direction([(X, Y), (X, Y-1) |_], left).
+get_direction([(X, Y), (X, Y+1) |_], right).
+
 is_dirt(X, Y, Dirtlist):- member((X, Y), Dirtlist).
 inside_enviroment(X, Y, N, M):-
 	X>=0,X<N, 
@@ -44,10 +50,55 @@ bfs(PathsQueue, N, M, Obstacles, GoalsList, Solution):-
 %=====================================================================+
 
 % Robot Agent:
-next_move((X, Y), _, _, _, _, Childs, Corrals, [drop]):-
-	member((X, Y), Childs),
-	member((X, Y), Corrals).
 
-next_move((X, Y), N, M, Dirts, Childs, Obstacles, _, [clean]):-
+%	Dirt:
+% 		Not carrying a child, enviroment is very dirty: Find dirt to clean
+next_move((X, Y), N, M, Carrying, Dirts, Childs, Obstacles, _, [Move]):-
+	Carrying = false,
+	not(is_very_dirty(Dirts, Childs, Obstacles, N, M)),
+	bfs([[(X, Y)]], N, M, Obstacles, Childs, Path),
+	get_direction(Path, Move),
+	!.
+
+% 		Not carrying a child, enviroment is very dirty, standing on dirt: Clean the dirt
+next_move((X, Y), N, M, Carrying, Dirts, Childs, Obstacles, _, [clean]):-
+	Carrying = false,
 	is_very_dirty(Dirts, Childs, Obstacles, N, M),
-	member((X, Y), Dirts).
+	member((X, Y), Dirts),
+	!.
+
+
+% 	Child:
+%		Caryying a child, enviroment isnt very dirty: Move towards closest corral to drop child (1 Step)
+next_move((X, Y), N, M, Carrying, Dirts, Childs, Obstacles, Corrals, [Move]):-
+	Carrying = true,
+	not(is_very_dirty(Dirts, Childs, Obstacles, N, M)),
+	append(Obstacles, Childs, RobotObstacles),
+	bfs([[(X, Y)]], N, M, RobotObstacles, Corrals, [S, T|PathTail]),
+	member(T, Corrals),
+	get_direction([S, T|PathTail], Move),
+	!.
+
+%		Caryying a child, enviroment isnt very dirty: Move towards closest corral to drop child (2 Step)
+next_move((X, Y), N, M, Carrying, Dirts, Childs, Obstacles, Corrals, [Move1, Move2]):-
+	Carrying = true,
+	not(is_very_dirty(Dirts, Childs, Obstacles, N, M)),
+	append(Obstacles, Childs, RobotObstacles),
+	bfs([[(X, Y)]], N, M, RobotObstacles, Corrals, [S, T, Y|PathTail]),
+	get_direction([S, T, Y|PathTail], Move1),
+	get_direction([T, Y|PathTail], Move2),
+	!.
+
+% 		Carrying a child, standing at corral: Drop child
+next_move((X, Y), _, _, Carrying, _, _, _, Corrals, [drop]):-
+	Carrying = true,
+	member((X, Y), Corrals),
+	!.
+
+% 		Not carrying a child, enviroment isnt very dirty: Move towards closest child to carry
+next_move((X, Y), N, M, Carrying, Dirts, Childs, Obstacles, _, [Move]):-
+	Carrying = false,
+	not(is_very_dirty(Dirts, Childs, Obstacles, N, M)),
+	bfs([[(X, Y)]], N, M, Obstacles, Childs, Path),
+	get_direction(Path, Move),
+	!.
