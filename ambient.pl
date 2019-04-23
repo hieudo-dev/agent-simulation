7f6e05d15c11cfe99583c63a3c65b827382b3986
Joel David Hernandez Cruz <jdavidhc1710@gmail.com>
2019-04-23 19:31:07 -0400

    Add move_child

--- ambient.pl
+++ ambient.pl
@@ -8,6 +8,11 @@
 
 :-dynamic dirt/2, robot/3, child/3, obst/2, crib/2, board/2.
 
+% board(1,1).
+% obst(2,1).
+% child(3, 1, 2).
+
+
 
 % Auxiliar Methods
 % get_random ::= return: A random position of the list L.
@@ -23,6 +28,17 @@ adjacent(P, (NX, NY)):- arg(1, P, X), arg(2, P, Y), NX is X - 1, NY is Y, board(
 % checkMember ::= Verify what type the point is
 checkMember((X, Y), B):-member((X, Y), B).
 checkMemberWithId((X, Y, Id), B):- member((X, Y, Id), B).
+
+% validPosition(X,Y)
+validPosition((X, Y)):- listBoard(L), listDirt(D), 
+                        append(L, D, R), member((X,Y), R), !. 
+
+writeChar((X, Y)):- listBoard(L), member((X, Y), L), write(.), tab(1), !.
+writeChar((X, Y)):- listObst(O), member((X, Y), O), write("O"), tab(1), !.
+writeChar((X, Y)):- listDirt(D), member((X, Y), D), write("D"), tab(1), !.
+writeChar((X, Y)):- listCrib(C), member((X, Y), C), write("C"), tab(1), !.
+writeChar((X, Y, Id)):- listChild(Ch), member((X, Y, Id), Ch), write("Ch"), tab(1), !.
+
 
 
 
@@ -57,8 +73,7 @@ listBoard(B):-findall((X, Y), board(X, Y), B).
 listDirt(D):-findall((X,Y), dirt(X,Y), D).
 listCrib(C):-findall((X,Y), crib(X,Y), C).
 listObst(O):-findall((X,Y), obst(X,Y), O).
-listChild(Ch):-findall((X,Y), child(X, Y, _), Ch).
-get_robot(P):-findall((X,Y), robot(X, Y, 1), P).
+listChild(Ch):-findall((X, Y, Id), child(X, Y, Id), Ch).
 
 % --------------------------------------------------------------------------------------------------------
 %   Crib Generation
@@ -98,19 +113,19 @@ generate_dirt(N):- listBoard(L), get_random(L, Pa), arg(1, Pa, X), arg(2, Pa, Y)
 % --------------------------------------------------------------------------------------------------------
 
 generate_child(1):- listBoard(L), listDirt(D), append(L, D, R), get_random(R, Pa), 
-                    arg(1, Pa, X), arg(2, Pa, Y), member((X,Y), L), retract(board(X,Y)), 
-                    assert(child(X,Y)), !.
+                    arg(1, Pa, X), arg(2, Pa, Y), member((X,Y), L), !, retract(board(X,Y)), 
+                    assert(child(X, Y, 2)), !.
 
 generate_child(1):- listBoard(L), listDirt(D), append(L, D, R), get_random(R, Pa), 
-                    arg(1, Pa, X), arg(2, Pa, Y), member((X,Y), D), assert(child(X,Y)), !.
+                    arg(1, Pa, X), arg(2, Pa, Y), member((X,Y), D), !, assert(child(X, Y, 2)), !.
 
-generate_child(N):- listBoard(L), listDirt(D), append(L, D, R), get_random(R, Pa), 
-                    arg(1, Pa, X), arg(2, Pa, Y), member((X,Y), L), retract(board(X,Y)), 
-                    assert(child(X,Y)), NS is N - 1, generate_child(NS).
+generate_child(N):- listBoard(L), listDirt(D), listChild(Ch), append(L, D, R), get_random(R, Pa), 
+                    arg(1, Pa, X), arg(2, Pa, Y), member((X,Y), L), not(member((X, Y), Ch)), !, retract(board(X,Y)), 
+                    Id is N + 1, assert(child(X, Y, Id)), NS is N - 1, generate_child(NS).
 
-generate_child(N):- listBoard(L), listDirt(D), append(L, D, R), get_random(R, Pa), 
-                    arg(1, Pa, X), arg(2, Pa, Y), member((X,Y), D), 
-                    assert(child(X,Y)), NS is N - 1, generate_child(NS).
+generate_child(N):- listBoard(L), listDirt(D), listChild(Ch), append(L, D, R), get_random(R, Pa), 
+                    arg(1, Pa, X), arg(2, Pa, Y), member((X,Y), D), not(member((X, Y), Ch)), !, 
+                    Id is N + 1, assert(child(X, Y, Id)), NS is N - 1, generate_child(NS).
 
 % --------------------------------------------------------------------------------------------------------
 %    Robot Generation 
@@ -121,15 +136,16 @@ generate_robot:- listBoard(L), get_random(L, Pa), arg(1, Pa, X), arg(2, Pa, Y), 
 
 
 % Render Board
-% render_board(1, 1, B, D, C, O, CH):- .
-% 
+render_board(L, 1):- nth1(1, L, Elem), writeChar(Elem), nl, !.
+render_board(L, P):- nth1(P, L, Elem), writeChar(Elem), Ps is P - 1, render_board(L, Ps).
 
-% paint(X,Y):- listBoard(B), checkMember((X, Y), B), write(.), tab(1).
-% paint(X,Y):- listBoard(D), checkMember((X, Y), D), write(#), tab(1).
-% paint(X,Y):- listBoard(O), checkMember((X, Y), O), write(O), tab(1).
-% paint(X,Y):- listBoard(S), checkMember((X, Y), S), write(█), tab(1).
-% paint(X, Y, Id):- listBoard(Ch), checkMemberWithId((X, Y, Id), Ch), write(.), tab(1).
-% paintRobot:- write(®), tab(1).
+% Get Elemnts in the N-esima row
+get_boards(N, L):-findall((N, Y), board(N, Y), L).
+get_obsts(N, L):-findall((N, Y), obst(N, Y), L).
+get_dirts(N, L):-findall((N, Y), dirt(N, Y), L).
+get_cribs(N, L):-findall((N, Y), crib(N, Y), L).
+get_childs(N, T, L):-findall((N, Y, T), child(N, Y, T), L).
+get_robot(N, P):-findall((N, Y, 1), robot(N, Y, 1), P).
 
 
 % --------------------------------------------------------------------------------------------------------
@@ -142,3 +158,14 @@ generate_robot:- listBoard(L), get_random(L, Pa), arg(1, Pa, X), arg(2, Pa, Y), 
 %   
 %   
 % --------------------------------------------------------------------------------------------------------
+
+move_child(Id, (Xc, Yc), (NX, NY)):- validPosition((NX, NY)), retract(child(Xc, Yc, Id)), 
+                                    assert(board(Xc, Yc)), assert(child(NX, NY, Id)).
+move_child(Id, (Xc, Yc), (NX, NY)):- move_obst((Xc, Yc), (NX, NY)), retract(child(Xc, Yc, Id)),
+                                     assert(board(Xc, Yc)), retract(obst(2,1)), assert(child(NX, NY, Id)).
+
+
+move_obst((_, _), (Xa, Ya)):- listBoard(L), member((Xa, Ya), L), retract(board(Xa, Ya)), assert(obst(Xa, Ya)), !.
+move_obst((X, Y), (Xa, Ya)):- Dx is Xa - X, Dy is Ya - Y, 
+                              Nx is Xa + Dx, Ny is Ya + Dy,
+                              listObst(L), not(member((Nx, Ny), L)), move_obst((Xa, Ya), (Nx, Ny)). 
